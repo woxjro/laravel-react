09c0487e9f6929114a3ca29ad16442dacdf057c0
woxjro <n0i6c0k2@gmail.com>
2020-08-22 22:35:13 +0900

    routerを試してみる。

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,21 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import About from "./About";
+import User from "./User";
+import Top from "./Top";
 
 export default function App() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">
-                            あああーーーーー！！！
-                        </div>
+        <Router>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">react-router-dom</div>
+                            <div className="card-body">
+                                <div className="nav">
+                                    <Link to="./about">
+                                        <li>about</li>
+                                    </Link>
+                                    <Link to="./user">
+                                        <li>user</li>
+                                    </Link>
+                                </div>
 
-                        <div className="card-body">hi!!!</div>
+                                <Switch>
+                                    <Route path="/" exact component={Top} />
+                                    <Route path="/about" component={About} />
+                                    <Route path="/user" component={User} />
+                                </Switch>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Router>
     );
 }
 
---
+++ resources/js/components/Top.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Top() {
+    return <div>top page</div>;
+}
